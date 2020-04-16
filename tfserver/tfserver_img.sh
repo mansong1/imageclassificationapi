bca77e3a4947abccb6acb47d2824d14b8ee92ef6
mansong1 <martin.ansong@gmail.com>
2020-04-16 08:35:56 +0100

    Add prediction print out

--- tfserver/tfserver_img.sh
+++ tfserver/tfserver_img.sh
@@ -3,16 +3,23 @@
 # Creates tfserver based on saved model
 # Author: Martin Ansong <martin.ansong@gmail.com>
 
-mkdir -p /tmp/resnet
+"""
+./tfserver resnet http://download.tensorflow.org/models/official/20181001_resnet/savedmodels/resnet_v2_fp32_savedmodel_NHWC_jpg.tar.gz
+"""
+
+MODEL=$1
+URL=$2
+
+mkdir -p /tmp/${MODEL}
 
 # Download pretrained model
-curl -s http://download.tensorflow.org/models/official/20181001_resnet/savedmodels/resnet_v2_fp32_savedmodel_NHWC_jpg.tar.gz | \
-tar --strip-components=2 -C /tmp/resnet -xvz
+curl -s ${URL} | \
+tar --strip-components=2 -C /tmp/${MODEL} -xvz
 
 docker run -d --name serving_base tensorflow/serving
 # Move model to base image
 docker cp /tmp/resnet serving_base:/models/resnet
-docker commit --change "ENV MODEL_NAME resnet" serving_base \
+docker commit --change "ENV MODEL_NAME ${MODEL}" serving_base \
   $USER/resnet_serving
 docker kill serving_base
 docker rm serving_base
